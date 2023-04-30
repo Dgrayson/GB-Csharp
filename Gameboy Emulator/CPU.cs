3d1c0447800599854b2a7683cffdd4df402a8df0
dgrayson <dgrayson100@gmail.com>
2023-04-30 14:39:43 -0400

    pushing updates

--- Gameboy Emulator/CPU.cs
+++ Gameboy Emulator/CPU.cs
@@ -29,10 +29,28 @@ namespace Gameboy_Emulator
             byte opcode = _mmu.ReadByte(PC++);
 
             switch (opcode)
-            { 
-                
-            }
+            {
+                case 0x40: { B = B; break; }
+                case 0x41: { B = C; break; }
+                case 0x42: { B = D; break; }
+                case 0x43: { B = E; break; }
+                case 0x44: { B = H; break; }
+                case 0x45: { B = L; break; }
+                //case 0x46: { B = HL; break; }
 
+                // Additon 
+                case 0x80: { A += B;    break; }
+                case 0x81: { A += C;    break; }
+                case 0x82: { A += D;    break; }
+                case 0x83: { A += E;    break; }
+                case 0x84: { A += H;    break; }
+                //case 0x85: { A += HL;   break; }
+                case 0x86: { A += A;    break; }  
+                default: { break; } 
+            }
         }
+
+        public void LoadRegister() { }
+        public void LoadRegisterImmediate() { }
     }
 }
